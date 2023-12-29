7ed7bec6ac7a898c3fe456d82ac28df8361a6aba
Wouter de Bruijn <wouter@hedium.nl>
2023-12-29 21:34:54 +0100

    ✨ Added SlopedContainer element

---
+++ components/SlopedContainer.tsx
@@ -0,0 +1,30 @@
+import { JSX } from "preact/jsx-runtime";
+
+export function SlopedContainer(props: JSX.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      {...props}
+      class="bg-background relative py-24 flex justify-center items-center"
+    >
+      {/* Above triangle */}
+      <div
+        class="absolute top-0 left-0 w-full h-16 bg-primary"
+        style={{
+          clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 20%)",
+        }}
+      />
+
+      <div class="container px-8">
+        {props.children}
+      </div>
+
+      {/* Below triangle inverse */}
+      <div
+        class="absolute bottom-0 left-0 w-full h-16 bg-primary"
+        style={{
+          clipPath: "polygon(0 100%, 100% 100%, 100% 0, 0 80%)",
+        }}
+      />
+    </div>
+  );
+}
