be37e6e5a7d0f54252e70b41334bbf3854668aa7
Wouter de Bruijn <wouter@hedium.nl>
2023-12-30 17:46:01 +0100

    🔧 Fresh generation updates

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,9 +4,9 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
-import * as $api_joke from "./routes/api/joke.ts";
-import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
+import * as $projects_slug_image_ from "./routes/projects/[slug]/[image].tsx";
+import * as $projects_slug_index from "./routes/projects/[slug]/index.tsx";
 import * as $Header from "./islands/Header.tsx";
 import { type Manifest } from "$fresh/server.ts";
 
@@ -14,9 +14,9 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
-    "./routes/api/joke.ts": $api_joke,
-    "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
+    "./routes/projects/[slug]/[image].tsx": $projects_slug_image_,
+    "./routes/projects/[slug]/index.tsx": $projects_slug_index,
   },
   islands: {
     "./islands/Header.tsx": $Header,
