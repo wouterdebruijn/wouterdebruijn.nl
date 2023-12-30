730a744f31a3b9d870e7c213c94796ae7e124938
Wouter de Bruijn <wouter@hedium.nl>
2023-12-31 00:05:56 +0100

    💄 Fixed slope on smaller screens

--- components/SlopedContainer.tsx
+++ components/SlopedContainer.tsx
@@ -8,7 +8,7 @@ export function SlopedContainer(props: JSX.HTMLAttributes<HTMLDivElement>) {
     >
       {/* Above triangle */}
       <div
-        class="absolute top-0 left-0 w-full h-16 bg-primary"
+        class="absolute top-0 left-0 w-full h-16 bg-primary min-w-[100rem]"
         style={{
           clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 20%)",
         }}
@@ -20,7 +20,7 @@ export function SlopedContainer(props: JSX.HTMLAttributes<HTMLDivElement>) {
 
       {/* Below triangle inverse */}
       <div
-        class="absolute bottom-0 left-0 w-full h-16 bg-primary"
+        class="absolute bottom-0 left-0 w-full h-16 bg-primary min-w-[100rem]"
         style={{
           clipPath: "polygon(0 100%, 100% 100%, 100% 0, 0 80%)",
         }}
