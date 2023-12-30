36bcf76a2442eb0fea7ff5b2caa1c95b8ca49666
Wouter de Bruijn <wouter@hedium.nl>
2023-12-31 00:21:31 +0100

    ♻️ Added content to home page

--- routes/index.tsx
+++ routes/index.tsx
@@ -18,7 +18,7 @@ export const handler: Handlers<Omit<Project, "content">[]> = {
 export default function Home({ data }: PageProps<Omit<Project, "content">[]>) {
   function ProjectList() {
     return (
-      <div class="mt-16 grid gap-2 container grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+      <div class="grid gap-2 container grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
         {data.map((project) => (
           <ProjectCard
             title={project.title}
@@ -42,20 +42,21 @@ export default function Home({ data }: PageProps<Omit<Project, "content">[]>) {
             <MonitorIllustration class="max-w-xs lg:max-w-full" />
           </div>
           <GenericText title="~$ whois">
-            <p class="font-roboto">
+            <p class="font-roboto text-white">
               {/* About me text */}
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Similique sit cupiditate rem perferendis quo sint dignissimos quam
-              unde alias deserunt minima dolorem ipsa obcaecati earum vel sunt
-              tempore, consectetur quasi, nam minus, itaque deleniti distinctio!
-              Unde, iste incidunt voluptates vitae itaque quaerat quidem cum
-              aspernatur nemo repellat, consectetur quis rerum aut dolorem ipsum
-              sequi dolores totam illum quisquam neque nihil iusto ullam. Odio a
-              blanditiis officia unde ea iusto quisquam voluptatem obcaecati
-              veritatis, nesciunt possimus facere ducimus doloremque quaerat!
-              Quia eveniet sint eum fugiat repudiandae, nisi fugit
-              reprehenderit, quas similique dicta maiores ea quibusdam esse,
-              saepe tempora excepturi praesentium minus!
+              I am Wouter de Bruijn a 22 year old guy who is interested in all
+              kinds of technology. I experiment with Linux servers, computer
+              networking, software development, embedded devices and other
+              various bits of technology.
+            </p>
+            <p class="font-roboto text-white pt-2">
+              I have been interested in technology since I was a little kid. At
+              first mostly in physical things like basic electronics and making
+              stuff out of wood and scraps. Later on I got interested in
+              computers and software development. I started out with making some
+              simple websites and setting up a Linux server. Which grew into
+              following two IT educations, and working part-time as a software
+              developer and network/ system engineer for two companies.
             </p>
           </GenericText>
         </div>
@@ -63,21 +64,42 @@ export default function Home({ data }: PageProps<Omit<Project, "content">[]>) {
 
       <GenericContainer class="bg-primary">
         <div class="items-center grid gap-8 grid-cols-1 lg:grid-cols-2 lg:gap-40">
-          <GenericText title="~$ cd ~/Projects">
-            <p class="font-roboto text-black">
-              {/* About me text */}
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Similique sit cupiditate rem perferendis quo sint dignissimos quam
-              unde alias deserunt minima dolorem ipsa obcaecati earum vel sunt
-              tempore, consectetur quasi, nam minus, itaque deleniti distinctio!
-              Unde, iste incidunt voluptates vitae itaque quaerat quidem cum
-              aspernatur nemo repellat, consectetur quis rerum aut dolorem ipsum
-              sequi dolores totam illum quisquam neque nihil iusto ullam. Odio a
-              blanditiis officia unde ea iusto quisquam voluptatem obcaecati
-              veritatis, nesciunt possimus facere ducimus doloremque quaerat!
-              Quia eveniet sint eum fugiat repudiandae, nisi fugit
-              reprehenderit, quas similique dicta maiores ea quibusdam esse,
-              saepe tempora excepturi praesentium minus!
+          <GenericText title="~$ cat experience">
+            <p class="font-roboto">
+              I am currently studying Network and System Engineering at the
+              Hague University of Applied Sciences. I also work as a software
+              developer at{" "}
+              <a class="text-tertiary" href="https://hanexservice.eu/">
+                Hanex Service
+              </a>{" "}
+              and as a network/ system engineer at{" "}
+              <a class="text-tertiary" href="https://yieldergroup.com/">
+                Yielder Group
+              </a>.
+            </p>
+            <p class="font-roboto pt-2">
+              I started studying IT at{" "}
+              <a class="text-tertiary" href="https://novacollege.nl/">
+                Nova College
+              </a>{" "}
+              in 2017, where in 2020 I graduated and got my "ICT-Beheerder"
+              (MBO-4) degree. After that I started studying Network and System
+              Engineering at the{" "}
+              <a class="text-tertiary" href="https://www.thuas.com/">
+                Hague University of Applied Sciences
+              </a>.
+            </p>
+            <p class="font-roboto">
+              As part of my studies I have done a few internships, which
+              continued into some part-time jobs. More information about my
+              education and work expierence can be found on my{" "}
+              <a
+                class="text-tertiary"
+                href="https://www.linkedin.com/in/wouter-de-bruijn-b1a20115b/"
+              >
+                linkedin
+              </a>{" "}
+              profile.
             </p>
           </GenericText>
           <div class="flex flex-col items-center">
@@ -85,7 +107,9 @@ export default function Home({ data }: PageProps<Omit<Project, "content">[]>) {
           </div>
         </div>
 
-        <ProjectList />
+        <GenericText class="pt-10" title="~$ ls Projects">
+          <ProjectList />
+        </GenericText>
       </GenericContainer>
     </>
   );
