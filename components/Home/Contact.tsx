c97ed86c005f772056640f74165a1f766418a54e
Wouter de Bruijn <wouter@hedium.nl>
2024-10-17 09:56:58 +0200

    💄 Cleaned up contact element

--- components/Home/Contact.tsx
+++ components/Home/Contact.tsx
@@ -4,16 +4,17 @@ import { SlopedContainer } from "../SlopedContainer.tsx";
 export function Contact() {
   return (
     <SlopedContainer bottomSlope={false}>
-      <div class="items-center grid gap-8 grid-cols-1 lg:grid-cols-2 lg:gap-40">
-        <GenericText title="">
-          <p class="font-roboto text-white">
+      <GenericText title="">
+        <ul class="font-roboto text-white">
+          <li>Wouter de Bruijn</li>
+          <li>
             You can contact me by sending an email to{" "}
             <a class="text-tertiary" href="mailto:contact@wouterdebruijn.nl">
               contact@wouterdebruijn.nl
             </a>.
-          </p>
-        </GenericText>
-      </div>
+          </li>
+        </ul>
+      </GenericText>
     </SlopedContainer>
   );
 }
