e751d2683cc04914ed63b54847b2af1709fc0f64
Wouter de Bruijn <wouter@hedium.nl>
2025-06-04 09:28:52 +0200

    🔀 Merged development versions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,55 +3,47 @@ import Header from "../components/Header";
 import Experience from "@/components/Home/Experience";
 import Projects from "@/components/Home/Projects";
 import Contact from "@/components/Home/Contact";
-import { cookies } from 'next/headers'
 import { listProjects } from "@/utils/projects";
 
+import { cookies } from "next/headers";
 import { initPocketBase } from "@/lib/pocketbase";
-
-import { signOut } from "@/app/actions";
-import Form from "next/form";
 
 export default async function Home() {
   const cookieStore = await cookies();
-  const token = cookieStore.get('pb_auth')?.value;
+  const authCookieValue = cookieStore.get("pb_auth")?.value;
+  const authStoreJson = authCookieValue ? JSON.parse(authCookieValue) : null;
 
-  const pb = await initPocketBase(token ?? '');
+  const pb = await initPocketBase(authStoreJson);
+
+  const projectsDb = await pb.collection("projects").getList();
+  const projects = await listProjects();
 
   try {
     if (!pb.authStore.isValid) {
-      throw new Error('Invalid auth token');
+      throw new Error("Auth store is not valid");
+    } else {
+      await pb.collection("users").authRefresh();
     }
-    
-    await pb.collection('users').authRefresh()
-
-    console.log('Successfully refreshed auth token');
-  }
-  catch (e) {
-    console.error(e);
+  } catch (e) {
+    console.error("Error refreshing auth:", e);
+    pb.authStore.clear();
   }
 
-  const projects = await listProjects();
-  const account = await pb.collection('users').getOne(pb.authStore.record?.id ?? '');
-  
   return (
     <div>
       <Header />
-
-      <Form
-        className="absolute top-0 right-0 p-4 font-roboto text-lg"
-        action={signOut}
-      >
-        <p className="text-right">
-          Signed in as {account?.email}
-        </p>
-        <input type="submit" value="Sign out" />
-      </Form>
 
       <About />
 
       <Experience />
 
-      <Projects projects={projects} />
+      <Projects
+        projects={projects.filter((p) =>
+          projectsDb.items.some((i) => (i.slug === p.slug))
+        )}
+      />
+
+      { JSON.stringify(pb.authStore.record) }
 
       <Contact />
     </div>
