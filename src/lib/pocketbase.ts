98f348ba3071ad74346082b0c057af895e0bd947
Wouter de Bruijn <wouter@hedium.nl>
2025-06-04 22:39:23 +0200

    🔧 Changed env for pocketbase

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -7,7 +7,7 @@ interface AuthStoreDump {
 }
 
 export async function initPocketBase(authStore?: AuthStoreDump) {
-    const pb = new PocketBase(process.env.SERVICE_FQDN_POCKETBASE);
+    const pb = new PocketBase(process.env.POCKETBASE_URL);
 
     if (authStore) {
         pb.authStore.save(authStore?.token, authStore?.record);
