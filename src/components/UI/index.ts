c6f70807c030dcaee683be01109c0d1075d4622e
Wouter de Bruijn <wouter@hedium.nl>
2025-11-03 18:26:00 +0100

    🚚 Component renaming

--- src/components/UI/index.ts
+++
@@ -1,5 +0,0 @@
-// UI Components with named exports for better tree-shaking
-export { default as Button } from "./generic-button";
-export { default as GenericContainer } from "./generic-container";
-export { default as GenericText } from "./generic-text";
-export { default as SlopedContainer } from "./sloped-container";
