9e3bf1528503dc81a8641f103b14a17691e4e214
Wouter de Bruijn <wouter@hedium.nl>
2025-11-03 18:26:00 +0100

    🚚 Component renaming

--- src/components/Home/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as About } from "./about";
-export { default as Contact } from "./contact";
-export { default as Experience } from "./experience";
-export { default as ProjectCard } from "./project-card";
-export { default as Projects } from "./projects";
--- src/components/Projects/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ProjectComment } from "./project-comment";
-export { default as ProjectHeader } from "./project-header";
-export { default as ProjectImage } from "./project-image";
-export { default as ProjectTitle } from "./project-title";
