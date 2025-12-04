3bec755e08212277512b5327b5192f5cda99a262
Wouter de Bruijn <wouter@hedium.nl>
2025-12-04 09:39:09 +0100

    ⬆️ Updated to nextjs 16.0.7

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,5 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -9,8 +11,8 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "prettier", "next/typescript"),
-];
+const eslintConfig = [...nextCoreWebVitals, ...compat.extends("prettier"), ...nextTypescript, {
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}];
 
 export default eslintConfig;
